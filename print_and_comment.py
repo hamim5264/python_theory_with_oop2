bf28e1145a52f835711656ad4dddc0e8df5cd60c
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2024-02-19 19:53:43 +0600

    updated

---
+++ print_and_comment.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print("""Hello world""")
+print("'Hello world'")
+'''
+Here the quotation is not same
+That's why it will print the hello world with single quotation
+'''
+
+
+# For single line comment use hashtag
+
+'''
+For multiline comment use 3 single quotation in start and end
+'''
